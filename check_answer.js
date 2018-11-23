708e77796d8affb4b2f8bfef72fc8e62d30a9cf2
Peter Major <38843393+majorpiotr@users.noreply.github.com>
2018-11-23 01:10:17 +0100

    new progres bars

--- check_answer.js
+++ check_answer.js
@@ -7,6 +7,60 @@ $( document ).ready(function() {
 try_number = 1;
 question_numer=1;
 past_answears = [];
+progress_array = [0, 0];
+
+//data: [20, 10];
+var ctx = $(".myChart");
+var myBarChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Answer's"],
+        datasets: [{
+            label: '# of Votes',
+            data: progress_array,
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+
+    /*,
+    options: {
+       /* scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    barPercentage: 1
+                }
+            }]
+        }*/
+        /*
+        scales: {
+            xAxes: [{
+                stacked: true,
+                ticks: {
+                max:100,	
+                beginAtZero: true
+            }
+            }],
+            yAxes: [{
+                stacked: true,
+                ticks: {
+                max:100,
+                beginAtZero: true
+            }
+            }]
+        }
+    }
+*/
+   // options: options
+});
 function reset_clock()
 {
 	start_date = new Date(Date.now()) ;
@@ -57,6 +111,7 @@ function mark_speeling( ans_a , ans_b )
 */ 
 function go_to_next(question_numer)
 {
+	UIkit.countdown("#countdown_clock").stop(); // stop the clock during loading next question
 	question_url = "questions_text/";
 	question_url += question_numer;
 	question_url += ".xml";
@@ -69,34 +124,67 @@ function go_to_next(question_numer)
    	$( "#to_do").text($(xml).find("to_do").text());
    	$( "#question_img").attr("src",$(xml).find("img_src").text());
    	correct_answer=$(xml).find("correct").text();
-   	reset_clock();
+   	UIkit.countdown("#countdown_clock").start();//it starts the clock again, since new question has been loaded
     }})
+
+}
+
+function draw_minichart()
+{
+	var ctx_copy = $(".myBarChart_copy");
+	var myBarChart_copy = new Chart(ctx_copy , {
+    type: 'doughnut',
+    data: {
+        labels: ["Answer's"],
+        datasets: [{
+            label: '# of Votes',
+            data: progress_array,
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }]
+    }});
 }
 function check_answear( ans_a, ans_b )
 {
 
 if (ans_a.toLowerCase().trim() == ans_b.toLowerCase().trim()) 
 {
-	UIkit.notification({message: ans_a + '  - is the correct answear. ' + '<br> So proud of you' + ' User_name', status: 'success'});
 	//alert("Your answear is correct :)");
 	past_answears = [];
 	$("#user_answear").val("");
-	val = parseInt($("#js-progressbar").attr("value"));
-	val += 10; 
-	$("#js-progressbar").attr("value",val);
+	//val = parseInt($("#js-progressbar").attr("value"));
+	//val += 10; 
+	//$("#js-progressbar").attr("value",val);
 	question_numer++;
 	go_to_next(question_numer);
-	if ($("#js-progressbar").attr("value") == 100) 
-	{
-	UIkit.notification({message:'Test has been done :)', status: 'danger'});	
-	}
+	progress_array[0]=progress_array[0]+10;
+	
+myBarChart.update();	
+	UIkit.notification({message: ans_a + '  - is the correct answear. ' + '<br> So proud of you' + ' User_name' + '<BR>'+  '<canvas class="myBarChart_copy" width="40" height="40"></canvas>' , status: 'success',timeout: 1000});
+ //$(".myBarChart_copy").append($(".myChart").html());
+
+draw_minichart();
+
+//$(".myBarChart_copy").html($(".myChart").clone());
+
+	//if ($("#js-progressbar").attr("value") == 100) 
+	//{
+	//UIkit.notification({message:'Test has been done :)', status: 'danger'});	
+	//}
 
 }
 else
 {
 	if (!past_answears.includes(ans_a))
 	{
-	UIkit.notification({message: ans_a + '  - is completely not correct', status: 'danger'});
+	UIkit.notification({message: ans_a + '  - is completely not correct', status: 'danger',timeout:1000});
 	if (try_number <= 3 )
 	{
 
@@ -108,14 +196,19 @@ else
 	else
 	{
 			$("#user_answear").val("");
-			UIkit.notification({message: 'You have answeared 3 times incorrectly. Lets skip this question', status: 'danger'});
+			UIkit.notification({message: 'You have answeared 3 times incorrectly. Lets skip this question' +  '<canvas class="myBarChart_copy" width="40" height="40"></canvas>', status: 'danger',pos: 'bottom-center',timeout:3000});
 			question_numer++;
 			go_to_next(question_numer);
+			progress_array[1]=progress_array[1]+10;
+
+	myBarChart.update();
+draw_minichart();
 	}
 	}
 	else
 	{
-					UIkit.notification({message: 'The definition of insanity is doing the same thing over and over again and expecting a different result.' + ans_a + '<br> ,  was not, and never will be the correct answear for this :(', status: 'danger'});
+					UIkit.notification({message: ans_a + '<br> was not, and never will be the correct answear for this :(', status: 'danger',pos: 'top-right'});
+					UIkit.notification({message: 'The definition of insanity is doing the same thing over and over again and expecting a different result.', status: 'danger',pos: 'top-left'});
 	}
 
 }
@@ -137,10 +230,6 @@ $("#user_answear").keypress(function (e) {
     return false;    //<---- Add this line
   }
 });
-
-
-
-
 
 //$("#question_img").load("question_img/dog.jpg");
 });
